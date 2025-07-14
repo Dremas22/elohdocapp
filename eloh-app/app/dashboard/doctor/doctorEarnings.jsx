358113cfd467545db51536e20bb963e74bfe3ea9
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2025-07-14 09:48:17 +0200

    Added docotor earnings component

---
+++ eloh-app/app/dashboard/doctor/doctorEarnings.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DooctorEarnings = () => {
+  return (
+    <div className="text-[#66e4ff] flex flex-col md:flex-row items-center justify-center gap-6 p-6 w-auto md:w-full">
+      <div className="bg-[#123158] p-6 rounded-xl shadow-lg w-full md:w-1/2 text-center">
+        <h1 className="text-lg font-semibold mb-2">Number of Consultations:</h1>
+        <h2 className="text-3xl font-bold">0</h2>
+      </div>
+      <div className="bg-[#123158] p-6 rounded-xl shadow-lg w-full md:w-1/2 text-center">
+        <h1 className="text-lg font-semibold mb-2">Total Monthly Earnings:</h1>
+        <h2 className="text-3xl font-bold">0</h2>
+      </div>
+    </div>
+  );
+};
+
+export default DooctorEarnings;
