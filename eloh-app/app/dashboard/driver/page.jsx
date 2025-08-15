83f37922268563e136a61f04779fc1a46201a2d8
Nelson Malgas <zmalgas69@gmail.com>
2025-08-15 10:38:33 +0200

    Fix driver map error & added loading state

--- eloh-app/app/dashboard/driver/page.jsx
+++ eloh-app/app/dashboard/driver/page.jsx
@@ -1,17 +1,26 @@
 "use client";
 
+import Loading from "@/components/Loading";
 import DriverMap from "@/components/maps/DriverMap";
 import Script from "next/script";
+import { useState } from "react";
 
 const DriversDashboard = () => {
+  const [mapsReady, setMapsReady] = useState(false);
+
   return (
     <div className="flex items-center justify-center w-full h-screen text-black">
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-        strategy="beforeInteractive" // or "afterInteractive"
-        onError={() => console.error("Google Maps script failed to load")}
+        strategy="afterInteractive"
+        onLoad={() => {
+          setMapsReady(true);
+        }}
+        onError={() => console.error("❌ Google Maps script failed to load")}
       />
-      <DriverMap />
+
+      {/* Only render DriverMap when Google Maps is ready */}
+      {mapsReady ? <DriverMap /> : <Loading message="Loading Map..." />}
     </div>
   );
 };
--- eloh-app/components/Loading.jsx
+++ eloh-app/components/Loading.jsx
@@ -10,7 +10,7 @@
  */
 const Loading = ({ message = "Loading your meeting room..." }) => {
   return (
-    <div className="flex h-screen items-center justify-center bg-white text-gray-800">
+    <div className="flex h-screen items-center justify-center bg-white text-gray-800 w-[80vw]">
       <div className="text-center">
         <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 mx-auto mb-4" />
         <p className="text-lg font-semibold">{message}</p>
