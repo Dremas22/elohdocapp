3c1a22efe1f7f2a030723bc249aec4f6ea99193f
Kiloechoalpha <letlhake.kv@gmail.com>
2025-10-23 10:41:12 +0200

    feat: make PatientCloseMeetingButton fully responsive and adjust mobile UI

--- eloh-app/components/video-conferencing/PatientCloseMeetingButton.jsx
+++ eloh-app/components/video-conferencing/PatientCloseMeetingButton.jsx
@@ -9,13 +9,13 @@ const PatientCloseMeetingButton = ({ onClick, disabled = false }) => {
             disabled={disabled}
             className={`
         fixed
-        bottom-2 right-4
-        md:bottom-5 md:right-10
+        bottom-2 right-2.5
+        md:bottom-2 md:right-20
         lg:bottom-2.5 lg:mr-102
         h-[6.26vh]
         bg-[#222020]
         text-white
-        py-3 px-6
+        py-3 px-4
         text-sm md:text-base
         font-semibold
         rounded-xl
@@ -25,8 +25,11 @@ const PatientCloseMeetingButton = ({ onClick, disabled = false }) => {
         z-50
       `}
         >
-            <IoExitOutline size={20} className="md:size-[23px]" />
-            <span className="hidden sm:inline">Leave</span>
+            {/* Icon hidden on mobile */}
+            <IoExitOutline size={20} className="hidden sm:flex md:size-[23px]" />
+
+            {/* Always show the text */}
+            <span>Leave</span>
         </button>
     );
 };
